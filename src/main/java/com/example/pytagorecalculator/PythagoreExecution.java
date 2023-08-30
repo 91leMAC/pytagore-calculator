35da646115983d2b26e60e27e47a03e94459017b
91leMAC <90leMAC@protonmail.com>
2023-08-30 11:37:45 +0200

    Fix bug when register nothing to the lenght answer

--- src/main/java/com/example/pytagorecalculator/PythagoreExecution.java
+++ src/main/java/com/example/pytagorecalculator/PythagoreExecution.java
@@ -1,44 +1,79 @@
 package com.example.pytagorecalculator;
 import java.util.Scanner;
 
-public class PythagoreExecution {
-        public static void main(String[] args) {
+
+public class PythagoreExecution
+{
+        public static void main(String[] args)
+        {
+
+            // Programm explainations
 
             // To user explainations
             System.out.println("This program will calculate the hypotenuse of a right triangle.");
             System.out.println("A will be the hypotenuse of the rectangle, so B and C are the variable that we \"know\"");
 
+
             // Set the variables
             int ac;
             int cb;
-
-
+            String acInput = "";
+            String cbInput = "";
             Scanner scanner = new Scanner(System.in);
-            // Run the code
-            System.out.println("Please reffer the value off the two \"know\" line in the triangle rectangle, rectangle in C");
+            String error = "Error you must answer something.";
+            int answerToAC = 0;
+            int answerToCB = 0;
+
+            // Request AC Lenght
+
+            while (acInput.isEmpty())
+            {
+                System.out.println("Please reffer the value off the two \"know\" line in the triangle rectangle, rectangle in C");
+
+                System.out.println("Mesure for ac lenght?");
+                acInput = scanner.nextLine();
+                if (acInput.isEmpty())
+                {
+                    System.out.println(error);
+                }
+                else
+                {
+
+                    ac = Integer.parseInt(acInput);
+                    System.out.println("AC length are " + ac + " right now !");
+
+                    answerToAC = ac * ac;
+
+                }
+            }
 
 
-            System.out.println("Mesure for ac lenght?");
-            String text = scanner.nextLine();
+            // Request CB lenght
+            while (cbInput.isEmpty())
+            {
+                System.out.println("Mesure for CB lenght?");
+                cbInput = scanner.nextLine();
+                if (cbInput.isEmpty())
+                {
+                    System.out.println(error);
+                }
+                else
+                {
+
+                    cb = Integer.parseInt(cbInput);
+                    System.out.println("AC lenght are " + cb + " right now !");
+
+                    answerToCB = cb * cb;
+
+                }
+            }
 
 
-                System.out.println("Please write an accepted number (without any letter)");
-
-                ac = Integer.parseInt(text);
-                System.out.println("AC length are " + ac + " right now !");
-
-            System.out.println("Mesure for CB lenght?");
-            text = scanner.nextLine();
-            cb = Integer.parseInt(text);
-            System.out.println("AC lenght are " +cb+ " right now !");
-
-
-            int answerToAC = ac * ac;
-            int answerToCB = cb * cb;
             int TheoremExecution = answerToAC + answerToCB;
 
+            // Convert the result with the square root method
             short result = (short) Math.sqrt(TheoremExecution);
             System.out.print("The value for AB is: " + result + "!");
 
         }
-    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,12 @@
 module com.example.pytagorecalculator {
+
+    // Dependecies for libraries
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-                        requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
     opens com.example.pytagorecalculator to javafx.fxml;
     exports com.example.pytagorecalculator;
 }
